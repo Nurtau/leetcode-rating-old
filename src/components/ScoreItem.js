d68ee09499c680cc1aee623b43b0e8c2b0783f78
Nurtau <nurtaut@gmail.com>
2021-05-28 01:12:24 +0600

    adding special colors to 1-3 places

--- src/components/ScoreItem.js
+++ src/components/ScoreItem.js
@@ -1,12 +1,34 @@
 import React from "react";
+import "./Winners.css";
 
 const ScoreItem = ({ username, link, score, place }) => {
+	let winnerWrapperClass;
+	switch (place) {
+		case 1:
+			winnerWrapperClass = "gold";
+			break;
+		case 2:
+			winnerWrapperClass = "silver";
+			break;
+		case 3:
+			winnerWrapperClass = "bronze";
+			break;
+		default:
+			winnerWrapperClass = "";
+	}
 	return (
-		<div className="item" style={{fontSize: "19px"}}>
+		<div className={`item ${winnerWrapperClass}`} style={{ fontSize: "19px" }}>
 			<div class="content">
-				<div class="header" style={{marginBottom:"15px", fontStyle: "italic"}}>{place}</div>
+				<div
+					className="header"
+					style={{ marginBottom: "15px", fontStyle: "italic" }}
+				>
+					<p className={winnerWrapperClass}>{place}</p>
+				</div>
 				<div className="ui left floated">
-					<a href={link}>{username}</a>
+					<a href={link}>
+						<p className={winnerWrapperClass}>{username}</p>
+					</a>
 				</div>
 			</div>
 			<div className="ui right floated">{score} points</div>
