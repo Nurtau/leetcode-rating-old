45bfba03f7c0323b158c1a20098b40ba1de4bf5a
Nurtau <nurtaut@gmail.com>
2021-05-28 01:28:09 +0600

    container class added

--- src/components/App.js
+++ src/components/App.js
@@ -3,11 +3,13 @@ import ScoreList from "./ScoreList";
 
 const App = () => {
 	return (
-		<div
-			className="ui inverted segment"
-			style={{ padding: "30px", minHeight: "100vh", borderRadius: "0px" }}
-		>
-			<ScoreList />
+		<div className="ui inverted segment">
+			<div
+				className="ui inverted container"
+				style={{ padding: "30px", minHeight: "100vh", borderRadius: "0px" }}
+			>
+				<ScoreList />
+			</div>
 		</div>
 	);
 };
