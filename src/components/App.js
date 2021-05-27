440af4d24d82474bb3cdb3ffaac499c41eba415d
Nurtau <nurtaut@gmail.com>
2021-05-27 17:50:50 +0600

    removing borderRadius

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,10 @@ import ScoreList from "./ScoreList";
 
 const App = () => {
 	return (
-		<div className="ui inverted segment"  style={{padding: "30px", minHeight: "100vh"}}>
+		<div
+			className="ui inverted segment"
+			style={{ padding: "30px", minHeight: "100vh", borderRadius: "0px" }}
+		>
 			<ScoreList />
 		</div>
 	);
